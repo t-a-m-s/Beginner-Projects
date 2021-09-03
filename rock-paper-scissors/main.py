796e6ede12f4ca76ee4336aaea94d714ff03ec27
Leah Leffingwell <biclightersandfirefighters@gmail.com>
2021-09-03 19:50:30 -0400

    Update main.py

--- rock-paper-scissors/main.py
+++ rock-paper-scissors/main.py
@@ -1,1 +1,34 @@
+import random
 
+choices = ["rock", "paper", "scissors"]
+winners = {
+    "rock": "scissors",
+    "paper": "rock",
+    "scissors": "paper"
+}
+while True:
+    start_game = input("(P)lay or (Q)uit?")
+    if start_game == 'Q' or start_game == "q":
+        print("Goodbye")
+        break
+    else:
+        while True:
+            computer_choice = random.choice(choices)
+            player_choice = input("Pick rock, paper, or scissors:")
+            if player_choice not in choices:
+                print("Please enter a valid choice")
+            else:
+                print("Computer chose:", computer_choice)
+                print("Player chose:", player_choice)
+                if player_choice == computer_choice:
+                    winner = "Draw"
+                    print(winner)
+                elif player_choice == winners[computer_choice]:
+                    winner = "Computer wins!"
+                    print(winner)
+                elif computer_choice == winners[player_choice]:
+                    winner = "Player wins!"
+                    print(winner)
+            ans = input("Do you want to play again? (y/n)?")
+            if ans == 'n' or ans == 'N':
+                break
